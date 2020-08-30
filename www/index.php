903fb9222e16ecbef8aad31d341402fc64f82bde
Peter Lindstrom <4271040+plindstrom@users.noreply.github.com>
2020-08-30 01:24:12 -0600

    Updated currently reading.

--- www/index.php
+++ www/index.php
@@ -9,7 +9,7 @@ Page_Header(0);
 		<p>Hello, my name is Peter and welcome to my personal web site.  When I&rsquo;m not busy at work I enjoy cooking (well, eating mostly), reading a good book, watching slightly too much TV and relaxing at home. Beyond this web site you can find me at the usual places like <a href="https://www.linkedin.com/in/pclindstrom">LinkedIn</a>.</p>
 		<p>How about some fun(ish) facts:</p>
 		<ul class="list-standard">
-			<li>I&rsquo;m currently reading <a href="https://www.goodreads.com/book/show/7677.Jurassic_Park"><em>Jurassic Park</em></a> by Michael Crichton</li>
+			<li>I&rsquo;m currently reading <a href="https://www.goodreads.com/book/show/44088167-the-raid"><em>The Raid</em></a> by Steven Konkoly</li>
 			<li>The weather at home is <?php echo Get_Wx(); ?></li>
 			<li>When I was born <?php echo Get_Fact(); ?></li>
 		</ul>
